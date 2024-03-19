ed340d47fbe6830810c69ca3d1de741e0c05db7c
Adi-Dal <147609209+Adi-Dal@users.noreply.github.com>
2024-03-19 15:51:03 -0500

    Update main.py with interactions and research page

--- aegisRx/backend/main.py
+++ aegisRx/backend/main.py
@@ -13,5 +13,14 @@ def login():
 def logout():
     #fill these in once firebase auth is working
 
+@app.route("/research")
+def research():
+    #fill these in once firebase auth is working
+
+@app.route("/interactions")
+def interactions():
+    #fill these in once firebase auth is working
+
+
 if __name__ == "__main__":
   app.run()
