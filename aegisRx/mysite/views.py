3a08cdbfe4e5636e232dbc6e71b8d5fcc77e5270
Adi-Dal <147609209+Adi-Dal@users.noreply.github.com>
2024-03-20 12:14:45 -0500

    Update views.py

--- aegisRx/mysite/views.py
+++ aegisRx/mysite/views.py
@@ -4,3 +4,11 @@ views = Blueprint('views', __name__)
 @views.route('/')
 def home():
     return render_template('index.html')
+
+@views.route("/research")
+def research():
+    #fill these in once firebase auth is working
+
+@views.route("/interactions")
+def interactions():
+    #fill these in once firebase auth is working
