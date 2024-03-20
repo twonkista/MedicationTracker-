957557fe201086c80b9a286fa32700279e734b09
twonkista <rkomati787@gmail.com>
2024-03-20 14:00:35 -0500

    grah she in ha mood

--- aegisRx/mysite/auth.py
+++ aegisRx/mysite/auth.py
@@ -1,9 +1,32 @@
-from flask import Blueprint
+from flask import Flask, Blueprint, render_template, session, redirect, request
+import pyrebase
 auth = Blueprint('auth', __name__)
+
+config ={
+    "apiKey": "AIzaSyDzB9AORkJQSKSnI1wZaMX0BXrkgl75SNA",
+    "authDomain": "aegisr-d8561.firebaseapp.com",
+    "projectId": "aegisr-d8561",
+    "storageBucket": "aegisr-d8561.appspot.com",
+    "messagingSenderId": "709511623951",
+    "appId": "1:709511623951:web:2e8d4a6ebc674900b9ba7a",
+    "measurementId": "G-5JPQGY471T",
+    "databaseURL":''
+}
+
+firebase = pyrebase.initialize_app(config)
+log = firebase.auth()
+
 
 @auth.route('/login')
 def login():
-    return 'login'
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        try:
+            user = auth.sign_in_with_email_and_password(username,password)
+        except:
+            return render_template('index.html')
+    return render_template('dashboard.html')
 
 @auth.route('/logout')
 def logout():
--- aegisRx/mysite/views.py
+++ aegisRx/mysite/views.py
@@ -4,3 +4,15 @@ views = Blueprint('views', __name__)
 @views.route('/')
 def home():
     return render_template('index.html')
+
+@views.route('/home')
+def homeback():
+    return render_template('index.html')
+
+@views.route('/loginPage')
+def signin():
+    return render_template('login.html')
+
+@views.route('/signupPage')
+def signup():
+    return render_template('signup.html')
